4b37c8cf88210874ad46093ccb7e4cc4a981bc1b
Tom Lin <gundomlegend@gmail.com>
2023-08-16 10:10:15 +0800

    feat: install core、evm、transport-react-native-ble

---
+++ packages/transport-react-native-ble/lib/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "RNBleTransport", {
+  enumerable: true,
+  get: function get() {
+    return _RNBleTransport["default"];
+  }
+});
+exports["default"] = void 0;
+
+var _RNBleManager = _interopRequireDefault(require("./RNBleManager"));
+
+var _RNBleTransport = _interopRequireDefault(require("./RNBleTransport"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var rnBleManager = new _RNBleManager["default"]();
+var _default = rnBleManager;
+exports["default"] = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJuYW1lcyI6WyJybkJsZU1hbmFnZXIiLCJSTkJsZU1hbmFnZXIiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBQTs7QUFJQTs7OztBQUZBLElBQU1BLFlBQVksR0FBRyxJQUFJQyx3QkFBSixFQUFyQjtlQUdlRCxZIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJOQmxlTWFuYWdlciBmcm9tICcuL1JOQmxlTWFuYWdlcic7XG5cbmNvbnN0IHJuQmxlTWFuYWdlciA9IG5ldyBSTkJsZU1hbmFnZXIoKTtcblxuZXhwb3J0IHsgZGVmYXVsdCBhcyBSTkJsZVRyYW5zcG9ydCB9IGZyb20gJy4vUk5CbGVUcmFuc3BvcnQnO1xuZXhwb3J0IGRlZmF1bHQgcm5CbGVNYW5hZ2VyO1xuIl19
