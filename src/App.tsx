53e4ab767e0dccc4a7934a90de1bacc24600d651
Eric Ting <erictingting2000@gmail.com>
2023-08-16 17:07:56 +0800

    feat: config redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
 import {DemoAppNavigator} from '@src/DemoAppNavigator';
+import {store} from '@src/features/store/store';
+import {Provider} from 'react-redux';
 
 export const App = () => {
   return (
-    <NavigationContainer>
-      <DemoAppNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <DemoAppNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
---
+++ src/features/store/types.ts
@@ -0,0 +1,4 @@
+export enum ReducerTypes {
+  ACCOUNT = 'account',
+  DEVICE = 'device',
+}
