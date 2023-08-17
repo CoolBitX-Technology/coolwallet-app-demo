de6f6a602c77b74ff4129c9ef1e9f311b04bbbbd
Tom Lin <gundomlegend@gmail.com>
2023-08-17 17:46:34 +0800

    feat: add shim

--- src/features/ble/BluetoothScanContainer.tsx
+++ src/features/ble/BluetoothScanContainer.tsx
@@ -1,6 +1,5 @@
-
-import {useNavigation} from '@react-navigation/native';
-import {BluetoothScanView} from '@src/features/ble/BluetoothScanView';
+import { useNavigation } from '@react-navigation/native';
+import { BluetoothScanView } from '@src/features/ble/BluetoothScanView';
 import { useDispatchDeviceNameChange } from '@src/features/store/device/DeviceActionHooks';
 import { useState } from 'react';
 
