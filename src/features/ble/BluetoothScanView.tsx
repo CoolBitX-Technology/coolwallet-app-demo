20431f88ee6c3cebba3a8a12f3249e0b0d776f68
Tom Lin <gundomlegend@gmail.com>
2023-08-17 10:40:00 +0800

    feat: 完成 ble wireframe

--- src/features/ble/BluetoothScanView.tsx
+++ src/features/ble/BluetoothScanView.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {FlatList, Text, ViewStyle, View} from 'react-native';
+import { FlatList, ViewStyle, View, Button, RefreshControl } from 'react-native';
 import styled from 'styled-components/native';
-import {BluetoothItem} from '@src/features/components/BluetoothItem';
+import { BluetoothItem } from '@src/features/components/BluetoothItem';
 
-const Description = styled(Text)`
-  flex: 1;
-  justify-content: center;
-  text-align: center;
-  padding: 16px;
-  background: gray;
+const ItemSeparator = styled(View)`
+  height: 0.5px;
+  background-color: gray;
+`;
+
+const ButtonLayout = styled(View)`
+  width: 100%;
+  height: 40px;
+  margin-top: 16px;
+  align-items: space-around;
+  justify-content: space-around;
+  flex-direction: row;
 `;
 
 interface Props {
@@ -16,27 +22,54 @@ interface Props {
   items: Array<string>;
   pairedItem?: string;
   selectedItem?: string;
+  isScaning?: boolean;
+  onStartScan?: () => void;
   onSelected?: (item: string) => void;
   onConnected?: (item: string) => void;
   onCanceled?: () => void;
 }
-export function BluetoothScanView({ style, items, pairedItem, selectedItem }: Props): JSX.Element {
+export function BluetoothScanView({
+  style,
+  items,
+  isScaning = false,
+  pairedItem,
+  selectedItem,
+  onStartScan,
+  onCanceled,
+  onConnected,
+  onSelected,
+}: Props): JSX.Element {
   return (
     <View style={style}>
-      <Description>{'CoolWallet Demo App Bluetooth Settings'}</Description>
       <FlatList
+        refreshControl={
+          <RefreshControl refreshing={isScaning} onRefresh={onStartScan} />
+        }
         data={items}
-        renderItem={({item}) => {
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        renderItem={({ item, index }) => {
           return (
             <BluetoothItem
+              index={index}
               rssi={-50}
-              deviceId="CWP065250"
+              deviceId={`CWP06525${item}`}
               deviceName="CoolWallet"
-              isPaired={false}
-              isSelected={false}
+              isPaired={item === pairedItem}
+              isSelected={item === selectedItem}
+              onSelected={onSelected}
             />
           );
-        }}></FlatList>
+        }}
+      />
+      <ItemSeparator />
+      <ButtonLayout>
+        <Button title="取消" onPress={onCanceled} />
+        <Button
+          disabled={!selectedItem}
+          title="開始配對"
+          onPress={() => selectedItem && onConnected?.(selectedItem)}
+        />
+      </ButtonLayout>
     </View>
   );
 }
