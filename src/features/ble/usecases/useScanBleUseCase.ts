85554536822d3d505899c6b75cdfd568e9ddb1f7
Tom Lin <gundomlegend@gmail.com>
2023-08-18 17:08:36 +0800

    feat: 加入 useScanBleUseCase

--- src/features/ble/usecases/useScanBleUseCase.ts
+++ src/features/ble/usecases/useScanBleUseCase.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Platform } from 'react-native';
-import { requestMultiple, PERMISSIONS } from 'react-native-permissions';
+import { requestMultiple, PERMISSIONS, RESULTS } from 'react-native-permissions';
 import { RNBleManager } from '@src/features/ble/RNBleManager';
 import { BleError, BleErrorCode, Device as BluetoothDevice, State } from 'react-native-ble-plx';
 
@@ -8,36 +8,32 @@ export function useScanBleUseCase() {
   const [isScaning, setIsScaning] = useState(false);
   const [scannedDevices, setScannedDevices] = useState<Array<BluetoothDevice>>([]);
   const [error, setError] = useState<BleError | undefined>();
-  const Permissions = useBlePermissions();
 
   const listener = async (bleError?: BleError, device?: BluetoothDevice) => {
-    if (!bleError) {
-      if (error) setError(undefined);
-      if (!device) return;
+    if (device) {
       if (scannedDevices.some((savedDevice) => savedDevice.id === device.id)) return;
       setScannedDevices(scannedDevices.concat(device));
-      return;
-    }
-    setIsScaning(false);
-    setError(bleError);
-    switch (bleError?.errorCode) {
-      case BleErrorCode.BluetoothUnauthorized:
-        const results = await requestMultiple(Permissions);
-        if (Object.values(results).filter((result) => result === 'granted').length !== Permissions.length) return;
-        // startScan();
-        break;
+    } else if (bleError) {
+      if (bleError !== error) setError(bleError);
+      setIsScaning(false);
+      if (bleError.errorCode === BleErrorCode.BluetoothUnauthorized) {
+        const permissions = getBlePermissions();
+        const results = await requestMultiple(permissions);
+        if (isAllPermissionGranted(results)) return startScan();
+      }
     }
   };
 
   const startScan = () => {
     setIsScaning(true);
+    setError(undefined);
     RNBleManager.getInstance().listen(listener);
   };
 
   useEffect(() => {
     RNBleManager.getInstance().addListener((state) => {
       console.log('useScanBleUseCase.addListener >>> state = ', state);
-      if (state !== State.PoweredOn && state !== State.PoweredOff) return;
+      if (isBleNotReady(state)) return;
       startScan();
     }, true);
     return () => RNBleManager.getInstance().stopListen();
@@ -51,7 +47,7 @@ export function useScanBleUseCase() {
   };
 }
 
-function useBlePermissions() {
+function getBlePermissions() {
   if (Platform.OS === 'ios') return [PERMISSIONS.IOS.BLUETOOTH_PERIPHERAL, PERMISSIONS.IOS.LOCATION_WHEN_IN_USE];
   return [
     PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
@@ -59,4 +55,13 @@ function useBlePermissions() {
     PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
     PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
   ];
+}
+
+function isAllPermissionGranted(permissionResults: Record<string, string>): boolean {
+  const permissions = getBlePermissions();
+  return Object.values(permissionResults).filter((result) => result === RESULTS.GRANTED).length !== permissions.length;
+}
+
+function isBleNotReady(state: State): boolean {
+  return state !== State.PoweredOn && state !== State.PoweredOff;
 }
