2211be9815fdeb37d1474ab7cb0a7e7830540b97
Tom Lin <gundomlegend@gmail.com>
2023-08-22 16:33:15 +0800

    refactor: characteristics 改到 transport 內實作

--- src/features/ble/RNBleManager.ts
+++ src/features/ble/RNBleManager.ts
@@ -1,15 +1,7 @@
 import { Transport, BleManager as CWBleManager, device as CWDevice } from '@coolwallet/core';
 import { delay } from '@coolwallet/core/lib/utils';
 import { RNBleTransport } from '@src/features/ble/RNBleTransport';
-import {
-  BleError,
-  BleErrorCode,
-  BleManager,
-  Device as BluetoothDevice,
-  Characteristic,
-  State,
-  Subscription,
-} from 'react-native-ble-plx';
+import { BleError, BleErrorCode, BleManager, Device as BluetoothDevice, State, Subscription } from 'react-native-ble-plx';
 
 export class RNBleError extends Error {
   errorCode: BleErrorCode;
@@ -17,13 +9,6 @@ export class RNBleError extends Error {
     super(message);
     this.errorCode = errorCode;
   }
-}
-
-interface CWServiceCharacteristics {
-  command?: Characteristic;
-  data?: Characteristic;
-  status?: Characteristic;
-  response?: Characteristic;
 }
 
 export class RNBleManager implements CWBleManager {
@@ -60,7 +45,9 @@ export class RNBleManager implements CWBleManager {
     const devices = await this.getBondDevices();
     const device = devices.find((device) => device.id === deviceId);
     if (!device) return undefined;
-    return new RNBleTransport(device);
+    const transport = new RNBleTransport(device);
+    await transport.initServiceCharacteristics();
+    return transport;
   }
 
   getScannedDevice() {
@@ -116,66 +103,6 @@ export class RNBleManager implements CWBleManager {
       throw new RNBleError(BleErrorCode.DeviceConnectionFailed, `checkDeviceConnection >>> connect device ${deviceId} failed.`);
   }
 
-  private async getCWServiceCharacteristics(connectedDevice: BluetoothDevice): Promise<CWServiceCharacteristics | null> {
-    const services = await connectedDevice.services();
-    const characteristics: Array<CWServiceCharacteristics | null> = await Promise.all(
-      services.map(async (service) => {
-        const { uuid } = service;
-        const serviceUuid = CWDevice.getInfosForServiceUuid(uuid);
-        if (!serviceUuid) return null;
-        const servicesCharacteristics = await connectedDevice?.characteristicsForService(uuid);
-        if (!servicesCharacteristics)
-          throw new RNBleError(
-            BleErrorCode.ServiceNotFound,
-            `getCWServiceCharacteristics >>> ${connectedDevice.name} service's characteristics are not found.`,
-          );
-        const command = servicesCharacteristics.find((ch) => ch.uuid === serviceUuid.writeUuid);
-        const data = servicesCharacteristics.find((ch) => ch.uuid === serviceUuid.dataUuid);
-        const status = servicesCharacteristics.find((ch) => ch.uuid === serviceUuid.checkUuid);
-        const response = servicesCharacteristics.find((ch) => ch.uuid === serviceUuid.readUuid);
-        const characteristics: CWServiceCharacteristics = {
-          command,
-          data,
-          status,
-          response,
-        };
-        // Check whether any characteristic is null
-        if (Object.values(characteristics).some((ch) => !ch)) return null;
-        return characteristics;
-      }),
-    ).then((chs) => chs.filter((ch) => ch));
-    return characteristics?.[characteristics.length - 1];
-  }
-
-  private checkServiceCharacteristic(serviceCharacteristic?: CWServiceCharacteristics | null) {
-    if (!serviceCharacteristic)
-      throw new RNBleError(BleErrorCode.CharacteristicNotFound, 'checkServiceCharacteristic >>> Characteristic is not found.');
-    if (!serviceCharacteristic?.command) {
-      throw new RNBleError(
-        BleErrorCode.CharacteristicNotFound,
-        'checkServiceCharacteristic >>> Command characteristic is not found.',
-      );
-    }
-    if (!serviceCharacteristic?.data) {
-      throw new RNBleError(
-        BleErrorCode.CharacteristicNotFound,
-        'checkServiceCharacteristic >>> Data characteristic is not found.',
-      );
-    }
-    if (!serviceCharacteristic?.status) {
-      throw new RNBleError(
-        BleErrorCode.CharacteristicNotFound,
-        'checkServiceCharacteristic >>> Status characteristic is not found.',
-      );
-    }
-    if (!serviceCharacteristic?.response) {
-      throw new RNBleError(
-        BleErrorCode.CharacteristicNotFound,
-        'checkServiceCharacteristic >>> Response characteristic is not found.',
-      );
-    }
-  }
-
   async connectById(deviceId: string, callback?: (device: BluetoothDevice, error?: BleError) => void): Promise<Transport> {
     let connectedDevice: BluetoothDevice;
     // 藍芽配對連線
@@ -195,19 +122,11 @@ export class RNBleManager implements CWBleManager {
       connectedDevice = await this.bleManager.connectToDevice(deviceId);
     }
     this.checkDeviceConnection(deviceId, connectedDevice);
-    const isConnected = await connectedDevice.isConnected();
-    if (!isConnected) {
-      connectedDevice = await connectedDevice.connect();
-    }
-    this.checkDeviceConnection(deviceId, connectedDevice);
-    // 藍芽尋找特徵
-    connectedDevice = await connectedDevice.discoverAllServicesAndCharacteristics();
-    console.log('RNBleManager.discoverAllServicesAndCharacteristics finish');
-    const serviceCharacteristic = await this.getCWServiceCharacteristics(connectedDevice);
-    this.checkServiceCharacteristic(serviceCharacteristic);
-    console.log('RNBleManager.convertToRNBleTransport finish');
+    // 初始化 Transport 物件
+    const transport = new RNBleTransport(connectedDevice);
+    await transport.initServiceCharacteristics();
     callback?.(connectedDevice);
-    return new RNBleTransport(connectedDevice);
+    return transport;
   }
 
   async connect(device: BluetoothDevice, disconnected?: (device: BluetoothDevice, error?: BleError) => void): Promise<Transport> {
