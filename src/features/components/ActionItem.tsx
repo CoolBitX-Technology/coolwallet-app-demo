ec439eb9f60c8c721956bd42f95a9cf49845d146
Eric Ting <erictingting2000@gmail.com>
2023-08-22 16:49:56 +0800

    feat: create ActionItem / KeyboardAwareView component

---
+++ src/features/components/ActionItem.tsx
@@ -0,0 +1,32 @@
+import { Text, View } from 'react-native';
+import { Button, Input, VStack } from 'native-base';
+
+interface ActionItemProps {
+  title: string;
+  isEditable?: boolean;
+  buttonText: string;
+  onPressButton: () => void;
+}
+export function ActionItem(props: ActionItemProps) {
+  const { title, isEditable = false, onPressButton, buttonText } = props;
+  return (
+    <View
+      style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start', marginVertical: 8, width: '100%' }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: '400' }}>{title}</Text>
+      <VStack space={2} justifyContent="center" alignContent="flex-start">
+        <Input
+          editable={isEditable}
+          w={'100%'}
+          size="sm"
+          mt="4px"
+          isDisabled={!isEditable}
+          style={{ backgroundColor: isEditable ? '#ffffff' : '#474545' }}
+        ></Input>
+        <Button onPress={onPressButton} size="sm" mt="4px">
+          {buttonText}
+        </Button>
+      </VStack>
+    </View>
+  );
+}
