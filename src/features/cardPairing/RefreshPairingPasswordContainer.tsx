64b7db0b6f35b32db5066af7b7ba79f61391ee6d
Eric Ting <erictingting2000@gmail.com>
2023-09-07 18:55:56 +0800

    chore: refine code

--- src/features/cardPairing/RefreshPairingPasswordContainer.tsx
+++ src/features/cardPairing/RefreshPairingPasswordContainer.tsx
@@ -2,26 +2,26 @@ import { RNApduManager } from '@src/features/ble/RNApduManager';
 import { DemoView } from '@src/features/components/DemoView';
 import { useInitApduEffect } from '@src/features/home/usecases/useCardPairingUseCase';
 import { useAppId, useDispatchChangePairedPassword, usePairedPassword } from '@src/features/store/account/AccountActionHooks';
-import { useBluetoothInfo, useIsConnected } from '@src/features/store/device/DeviceActionHooks';
+import { useCardId, useIsConnected } from '@src/features/store/device/DeviceActionHooks';
 import { useState } from 'react';
 
 export function RefreshPairingPasswordContainer() {
-  const bleInfo = useBluetoothInfo();
   const isConnected = useIsConnected();
   const changePairedPassword = useDispatchChangePairedPassword();
   const pairPassword = usePairedPassword();
   const appId = useAppId();
+  const cardId = useCardId();
   const [log, setLog] = useState('');
+  const isBtnDisable = !cardId || !isConnected || !pairPassword;
   const [isRefreshing, setIsRefereshing] = useState(false);
 
   useInitApduEffect();
 
-  const refreshPairPassword = async (cardId?: string) => {
-    if (!appId) return;
+  const refreshPairPassword = async () => {
+    if (!appId || isBtnDisable) return;
     setIsRefereshing(true);
     setLog('REFRESHING....');
     const newPairedPassword = await RNApduManager.getInstance().getPairPassword(appId);
-    if (!cardId) return;
     setIsRefereshing(false);
     setLog('REFRESHED SUCCESS');
     changePairedPassword(cardId, newPairedPassword);
@@ -31,8 +31,8 @@ export function RefreshPairingPasswordContainer() {
     <DemoView
       btnText="Refresh"
       log={log}
-      isBtnDisable={!isConnected || !pairPassword}
-      onPressBtn={() => refreshPairPassword(bleInfo?.cardId)}
+      isBtnDisable={isBtnDisable}
+      onPressBtn={() => refreshPairPassword()}
       textBoxBody={pairPassword}
       textBoxPlaceHolder="Your Pairing Password"
       isBtnLoading={isRefreshing}
