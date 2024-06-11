3c9752c8fd8a9cb06d745b328d5e81e7522bb59d
Tom Lin <gundomlegend@gmail.com>
2024-06-11 11:32:16 +0800

    feat: implement chage device info

--- src/features/httpScan/HttpScanContainer.tsx
+++ src/features/httpScan/HttpScanContainer.tsx
@@ -1,24 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import { HttpScanView } from '@src/features/httpScan/HttpScanView';
 import { useCreateHttpTransportUseCase } from '@src/features/httpScan/useCreateHttpTransportUseCase';
-import { useDispatchConnectStatus } from '@src/features/store/device/DeviceActionHooks';
+import { useHttpInfo } from '@src/features/store/device/DeviceActionHooks';
 import React, { useState } from 'react';
 
 const DEFAULT_HOSTNAME = '10.0.0.241';
 const DEFAULT_PORT = '9090';
 
 export function HttpScanContainer(): JSX.Element {
-  const changeConnectStatus = useDispatchConnectStatus();
-  const [hostname, setHostname] = useState(DEFAULT_HOSTNAME);
-  const [port, setPort] = useState(DEFAULT_PORT);
+  const httpInfo = useHttpInfo();
+  const [hostname, setHostname] = useState(httpInfo?.hostname || DEFAULT_HOSTNAME);
+  const [port, setPort] = useState(httpInfo?.port || DEFAULT_PORT);
 
   const { connect, log } = useCreateHttpTransportUseCase();
 
-  const connectByUrl = () => {
-    connect(`http://${hostname}:${port}`);
-    changeConnectStatus(true);
-  }
   const goBack = useNavigation().goBack;
+
+  const connectByUrl = () => {
+    connect(hostname, port);
+    goBack();
+  };
 
   return (
     <HttpScanView
--- src/features/httpScan/useCreateHttpTransportUseCase.ts
+++ src/features/httpScan/useCreateHttpTransportUseCase.ts
@@ -1,22 +1,24 @@
 import { Transport } from '@coolwallet/core';
 import { useLogUseCase } from '@src/features/home/usecases/useLogUseCase';
-import { useState } from 'react';
+import { useDispatchHttpInfo, useHttpInfo } from '@src/features/store/device/DeviceActionHooks';
+import { useEffect, useState } from 'react';
 import { createTransport } from 'transport-jre-http';
 
 export function useCreateHttpTransportUseCase() {
-  const [transport, setTransport] = useState<Transport>();
+  const transport = useHttpTransport();
+  const changeHttpInfo = useDispatchHttpInfo();
 
   const { log, addLog } = useLogUseCase();
 
-  const connect = (url: string) => {
+  const connect = (hostname: string, port: string) => {
+    if (!hostname || !port) {
+      addLog(`Invalid host name or port.`);
+      return;
+    }
+    const url = `http://${hostname}:${port}`;
     addLog(`Create a transport with: ${url} ...`);
-
-    createTransport(url)
-      .then((transport) => {
-        setTransport(transport);
-        addLog(`Successfully saved http transport, device: ${transport?.device?.name}`);
-      })
-      .catch(addLog);
+    changeHttpInfo(hostname, port);
+    addLog(`Successfully saved http transport, device: ${transport?.device?.name}`);
   };
 
   return {
@@ -24,4 +26,18 @@ export function useCreateHttpTransportUseCase() {
     connect,
     log,
   };
+}
+
+export function useHttpTransport() {
+  const httpInfo = useHttpInfo();
+  const [transport, setTransport] = useState<Transport>();
+  useEffect(() => {
+    if (!httpInfo) return;
+    createTransport(httpInfo.url)
+      .then(setTransport)
+      .catch((e) => {
+        console.log(`useHttpTransport >>> createTransport error=${e}`);
+      });
+  }, [httpInfo]);
+  return transport;
 }
